e5c41d945acd6e6cf7b4165dd9a747fdc83cde0f
Andrés Giraldo <afga199714@gmail.com>
2019-06-18 17:44:54 -0500

    admin design and requirements

--- recipesback/settings.py
+++ recipesback/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material.theme.blue',
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
